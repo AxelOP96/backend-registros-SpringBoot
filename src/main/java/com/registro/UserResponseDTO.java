c7caa4a41847aba1d77486b2b1f6f6d7b77470cc
Axel <axeelleguero@gmail.com>
2025-04-26 21:27:53 -0300

    FEAT(DTO) added DTOs

---
+++ src/main/java/com/registro/UserResponseDTO.java
@@ -0,0 +1,14 @@
+package com.formdev.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
