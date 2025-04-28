4a94734f91291f158ce7d362fff278a0890e6cf1
Axel <axeelleguero@gmail.com>
2025-04-27 21:14:22 -0300

    Deleted dtos and added security

--- src/main/java/com/registro/repository/UserRepository.java
+++ src/main/java/com/registro/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.registro.repository;
 
-import com.formdev.model.User;
+import com.registro.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
